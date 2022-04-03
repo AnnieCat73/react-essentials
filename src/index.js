3f34db841330916bec40f5e9b41f5cd3b4b9238d
Annie <devannie777@gmail.com>
2022-04-03 12:50:43 +0100

    App component set up

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
- React.createElement("h1", {style: {color: "blue"}}, "Heyyy Everyone!!"),
+ <App />,
   document.getElementById('root')
 );
 
