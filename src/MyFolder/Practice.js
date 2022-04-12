843fc57656b157e5a468c0d6de2078a1bdc08258
Annie <devannie777@gmail.com>
2022-04-12 12:41:19 +0100

    UseState practice

--- src/MyFolder/Practice.js
+++ src/MyFolder/Practice.js
@@ -1,4 +1,5 @@
-import React from 'react'
+//import React from 'react'
+import { useEffect, useState } from 'react'
 
 function Secret() {
   return <h1>Secret information</h1>
@@ -8,10 +9,27 @@ function Regular() {
   return <h2>Normal information</h2>
 }
 
-function Practice(props) {
+
+
+
+
+function Practice({authorized}) {
+  const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
   return (
     <>
-      {props.authorized ? <Secret /> : <Regular />}
+    <div>
+      {authorized ? <Secret /> : <Regular />}
+    </div>
+    <div>
+      <h2>Current emotion is {emotion} and {secondary}</h2>
+      <button onClick={() => setEmotion("sad")}>Make sad</button>
+      <button onClick={() => (setSecondary("crabby"))}>Make crabby</button>
+      <button onClick={() => setEmotion("frustrated")}>Make frustrated</button>
+      <button onClick={() => setEmotion("enthusiastic")}>Make enthusiastic</button>
+    </div>
+      
+
     </>
   )
 }
