6fb5a7b775ab509bff5535e52f19daf0f4c89ab3
Annie <devannie777@gmail.com>
2022-04-30 12:39:58 +0100

    Heading and List components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import TodoList from './todos/TodoList';
 import Practice from './MyFolder/Practice';
 import Timeapp from './todos/Timeapp';
+import Heading from './todos/Heading';
+import List from './todos/List';
 
 
 function App() {
 
   return (
     <div className="App">
+      <Heading />
+      <List />
       <TodoList />
       <Practice authorized={true} />
       <Timeapp />
