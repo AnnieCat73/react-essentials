f7036903e40e5046795698d231b2667b7b0a48e6
Annie <devannie777@gmail.com>
2022-05-01 12:29:13 +0100

    Math.js component

--- src/todos/List.js
+++ src/todos/List.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import pi, { doublePi, triplePi } from './math';
 
 function List() {
   return (
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <>
+      <ul>
+        <li>Bacon</li>
+        <li>Jamon</li>
+        <li>Noodles</li>
+      </ul>
+      <ul>
+        <li>{pi}</li>
+        <li>{doublePi()}</li>{/*must have ()  as function*/}
+        <li>{triplePi()}</li>
+      </ul>
+    </>
+
   );
 }
 
