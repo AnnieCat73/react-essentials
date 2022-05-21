20d05c609429e31d0832b2f662dcb43fe5f56bb9
Annie  Indreiten <devannie777@gmail.com>
2022-05-21 07:13:22 +0100

    Complex state component

--- src/App.js
+++ src/App.js
@@ -178,9 +178,49 @@ function App() {
     e.preventDefault();
   }
 
+  /*Complex*/
+  const [fullName, setFullname] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+
+    setFullname(prevValue => {
+      //console.log(prevValue);//so don't delete prev value
+      if (inputName === "fName") {
+        return {
+          fName: newValue,
+          lName: prevValue
+        };
+      } else if (inputName === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: newValue
+        }
+      }
+    })
+  }
+
+
   return (
 
     <div>
+
+      <section>
+        <div className="container">
+          <h1>Hello {fullName.fName} {fullName.lName}</h1>
+          <form>
+            <input onChange={handleChange} name="fName" placeholder="First Name" value={fullName.fName} />
+            <input onChange={handleChange} name="lName" placeholder="Last Name" value={fullName.lName} />
+            <button>Submit</button>
+          </form>
+        </div>
+      </section>
+
+
       <section>
         <div className="container">
           <h1>Hello {headingTexter}</h1>
@@ -197,6 +237,7 @@ function App() {
         <h1>Trips API</h1>
       </header>
       <main>
+
         <p>The documentation</p>
         <div>
           {endpoints.map(endpoint => (
