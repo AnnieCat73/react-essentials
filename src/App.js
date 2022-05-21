b7430347f079fdeb0d887780950cde27b80371e0
Annie  Indreiten <devannie777@gmail.com>
2022-05-21 12:33:27 +0100

    Complex React pracite with prevValue set up

--- src/App.js
+++ src/App.js
@@ -204,10 +204,29 @@ function App() {
     })
   }
 
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
 
   return (
 
     <div>
+      <section>
+        <div className="container">
+          <h1>
+            Hello {contact.fName} {contact.lName}
+          </h1>
+          <p>{contact.email}</p>
+          <form>
+            <input name="fName" placeholder="First Name" />
+            <input name="lName" placeholder="Last Name" />
+            <input name="email" placeholder="Email" />
+            <button>Submit</button>
+          </form>
+        </div>
+      </section>
 
       <section>
         <div className="container">
